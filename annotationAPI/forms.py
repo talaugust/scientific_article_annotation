e55dacbf328de8cb72520cad2c94d848ea4d57f0
taugust <talaugust6@gmail.com>
2020-02-24 10:26:35 -0800

    updated instructions

--- annotationAPI/forms.py
+++ annotationAPI/forms.py
@@ -24,11 +24,11 @@ class AnnotationHITForm(ModelForm):
             'comments': Textarea(attrs={'cols': 80, 'rows': 3})
         }
         labels = {
-            'is_lead': 'Is there a lead? If so, please highlight the sentences that make up the lead',
-         	'lead_interest':'Please highlight the lead and rate, on a scale from 1-5, 1 being not at all and 5 being extremely, how excited the lead made about the rest of the article?',
-          	'is_main_points_highlight': 'Check this box after highlighting the sentences with the main point(s) of the article	 ',
-           	'is_care_highlight': 'Did you highlight all \'Why should I care?\' sentences?' ,
-            'is_conclusion':'Is there a concluding sentence? If so, please highlight the sentences that make up this conclusion' ,
+            'is_lead': 'Is there a lead? If so, please highlight the sentences that make up the lead and label the highlight \'LEAD\'.',
+         	'lead_interest':'Please highlight the lead and rate, on a scale from 1-5, 1 being not at all and 5 being extremely, how excited the lead made you about the rest of the article.',
+          	'is_main_points_highlight': 'Check this box after highlighting the sentences with the main point(s) of the article with the label \'MAIN\'.',
+           	'is_care_highlight': 'Did you highlight all \'Why should I care?\' sentences with the label \'IMPACT\'?' ,
+            'is_conclusion':'Is there a concluding sentence? If so, please highlight the sentences that make up this conclusion with the label \'CONCLUSION\'.' ,
             'age': 'What is your age?',
             'english_prof': 'How proficient are you in English?',
             'gender': 'What is your gender?',
