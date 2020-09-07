040b0380cb1fcfa4430ca270ea19f8ce40a80b1c
taugust <talaugust6@gmail.com>
2020-09-07 10:49:23 -0700

    updated admin

--- annotationAPI/admin.py
+++ annotationAPI/admin.py
@@ -1,7 +1,20 @@
 from django.contrib import admin
 from .models import Annotation, Article, AnnotationHIT
 
+
 # Register your models here.
-admin.site.register(Annotation)
-admin.site.register(Article)
-admin.site.register(AnnotationHIT)
+@admin.register(Annotation)
+class AnnotationAdmin(admin.ModelAdmin):
+    list_display = ('user_id', 'article_id', 'text', 'quote', 'created')
+
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'uri', 'site')
+
+
+@admin.register(AnnotationHIT)
+class AnnotationHITAdmin(admin.ModelAdmin):
+    list_display = ('user_id', 'article_id', 'comments', 'created')
+
--- intra_article_survey/admin.py
+++ intra_article_survey/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Demographics, ArticleResponse
 
 # Register your models here.
+@admin.register(Demographics)
+class DemAdmin(admin.ModelAdmin):
+    list_display = ('age', 'gender', 'profession', 'HITid', 'created')
+
+
+@admin.register(ArticleResponse)
+class ArticleResponseAdmin(admin.ModelAdmin):
+    list_display = ('user_id', 'article_id', 'created')
